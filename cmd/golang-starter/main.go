a3fade921fe8979fd1a12a95e3141810c3dcd896
jorgeacortes <jorgeacortes@users.noreply.github.com>
2022-05-21 15:42:56 +0200

    Initial commit

---
+++ cmd/golang-starter/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/jorgeacortes/golang-starter/internal"
+)
+
+func main() {
+	internal.DoesSomethingAndReturn5()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jorgeacortes/golang-starter
+
+go 1.18
+
+replace github.com/jorgeacortes/golang-starter => ../golang-starter
